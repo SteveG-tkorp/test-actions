cba3c7a909dfe38575c9bbdadc3f89ccea93b7e9
SteveG-tkorp <steve@tkorp.com>
2025-02-18 14:25:21 +0100

    maj config et passage au info au lieu de clo

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,20 @@
-import { getInput, setFailed, setOutput } from "@actions/core";
+import { getInput, info, setFailed, setOutput } from "@actions/core";
 import { context } from "@actions/github";
 
 async function run() {
   try {
     const token = getInput("GITHUB_TOKEN", { required: true });
-    console.log("token", token);
+    info(`token: ${token}`);
 
     const PR = getInput("pr_number", { required: true });
-    console.log("PR", PR);
+    info(`PR: ${PR}`);
 
     setOutput("pr_updated", "blablabla");
   } catch (error: unknown) {
-    console.error(error);
     if (error instanceof Error) {
       setFailed(error.message);
     }
   }
 }
+
+run();
